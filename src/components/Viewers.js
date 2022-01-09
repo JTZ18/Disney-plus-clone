c34493741eb59dad77bdae437487e94e56673e95
JTZ18 <jon.taylorlim99@gmail.com>
2022-01-10 00:16:58 +0800

    UPDATE: added videos to viewers.js

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,18 +6,33 @@ function Viewers() {
         <Container>
             <Wrap>
                 <img src="/images/viewers-disney.png"/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-pixar.png"/>
+                <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+                    <source src='/videos/1564676714-pixar.mp4' type='video/mp4'/>
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-marvel.png"/>
+                <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+                    <source src='/videos/1564676115-marvel.mp4' type='video/mp4'/>
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-starwars.png"/>
+                <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+                    <source src='/videos/1608229455-star-wars.mp4' type='video/mp4'/>
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-national.png"/>
+                <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+                    <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4'/>
+                </video>
             </Wrap>
         </Container>
     )
@@ -36,17 +51,36 @@ const Container = styled.div`
 `
 
 const Wrap = styled.div`
+    padding-top: 56.25%;
     border-radius: 10px;
     border: 3px solid rgba(249,249,249, 0.1);
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     cursor: pointer;
     transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    overflow: hidden;
+    position: relative;
 
     img {
         width: 100%;
         height: 100%;
         object-fit: cover;
+        position: absolute;
+        inset: 0px;
+        display: block;
+        //transition: opacity 500ms ease-in-out 0s;
+        top: 0;
+        z-index: 1;
+
+    }
+
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
     }
 
     &:hover {
@@ -54,5 +88,9 @@ const Wrap = styled.div`
         border-color: rgba(249,249,249, 0.8);
         box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, 
         rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+
+        video {
+            opacity: 1;
+        }
     }
 `
